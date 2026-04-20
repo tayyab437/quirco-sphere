09637f472696bcd7f798ef098a436bc29d6dc0bf
tayyab437 <ac.repair.in.dubai3@gmail.com>
2026-04-20 09:46:48 +0500

    Updated package-lock.json

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
